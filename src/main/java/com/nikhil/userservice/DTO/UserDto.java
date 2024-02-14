79c36f866ef306e754bb0c7ec5c2dac301a4bc91
nikhil <nikhillajineni2@gmail.com>
2024-02-14 22:32:38 +0530

    "all auth by us"

--- src/main/java/com/nikhil/userservice/DTO/UserDto.java
+++ src/main/java/com/nikhil/userservice/DTO/UserDto.java
@@ -1,11 +1,28 @@
 package com.nikhil.userservice.DTO;
 
+import com.nikhil.userservice.Models.Role;
+import com.nikhil.userservice.Models.User;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class UserDto {
     private String name;
     private String email;
     private String password;
+    private List<Role> roles;
+    private boolean isEmailVerified;
 
+    public static UserDto from(User user) {
+        if (user == null) return null;
+
+        UserDto userDto = new UserDto();
+        userDto.email = user.getEmail();
+        userDto.name = user.getName();
+        userDto.roles = user.getRoles();
+        userDto.isEmailVerified = user.isEmailVerified();
+
+        return userDto;
+    }
 }
